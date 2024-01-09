d871356b527daa01c251c42a49a2cb60f0b58acd
Justin Dom <jaydom28@gmail.com>
2024-01-08 22:34:33 -0500

    fixed create_config script

--- create_config.sh
+++ create_config.sh
@@ -7,4 +7,4 @@ app_data_path="${here}/app_data"
 
 echo "Current directory: ${here}"
 echo "Creating a configuration file at: ${config_path}"
-printf "ENV: dev\nDBNAME: khmer_db.db\nDATAPATH: /home/justin/Documents/Projects/lute/lute-v3-khmer/app_data\n" > "${config_path}"
+printf "ENV: dev\nDBNAME: ${db_name}\nDATAPATH: ${app_data_path}\n" > "${config_path}"
